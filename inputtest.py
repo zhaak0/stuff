159c440cf054ca4432765a0ea47a7688a38d9fad
zhaak0 <zakriya.khan@bcestudent.com>
2026-09-09 15:54:36 +0100

    Add files via upload

---
+++ inputtest.py
@@ -0,0 +1,7 @@
+def WELCOME_MESSAGE():
+    print(f'welcome {name} who is {age} years old!')
+
+print("hi!")
+name=input("whats your name? ")
+age=int(input("how old are you? "))
+WELCOME_MESSAGE()
---
+++ integer somthing.py
@@ -0,0 +1,16 @@
+import math
+
+howmanybandsigot=int(5928475798753)
+print(howmanybandsigot)
+
+PercentageOfWater=float(howmanybandsigot/70)
+print(PercentageOfWater)
+
+print("('-')")
+
+x=int(input())
+
+circle=math.pi*x
+print(circle)
+
+print(math.pi)
