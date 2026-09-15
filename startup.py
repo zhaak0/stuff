d6aa31f9e4815b9a32952f45f3cddbcb116342dc
zhaak0 <zakriya.khan@bcestudent.com>
2026-09-15 16:58:51 +0100

    Add files via upload

---
+++ startup.py
@@ -0,0 +1,24 @@
+import time
+
+print("...")
+time.sleep(1)
+print("???: do you hear me?... survivor?..")
+time.sleep(1)
+username=input("what is your name?\n")
+
+print("ah yes",username)
+print("that checks out.")
+time.sleep(1)
+print("welcome to..")
+time.sleep(2)
+print("______ _____  ___ ______   _    _  ___   _      _   __")
+print("|  _  \  ___|/ _ \|  _  \ | |  | |/ _ \ | |    | | / / ")
+print("| | | | |__ / /_\ \ | | | | |  | / /_\ \| |    | |/ /")
+print("| | | |  __||  _  | | | | | |/\| |  _  || |    |    \ ")
+print("| |/ /| |___| | | | |/ /  \  /\  / | | || |____| |\  \ ")
+print("|___/ \____/\_| |_/___/    \/  \/\_| |_/\_____/\_| \_/")
+
+print("[¬º-°]¬")
+
+print("wasd for movement")
+print("")
