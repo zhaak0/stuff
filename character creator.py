18898cfef35bfe24bfd7abbb8bef13010dc2ff79
zhaak0 <zakriya.khan@bcestudent.com>
2026-09-22 16:25:32 +0100

    Delete character creator.py

--- character creator.py
+++
@@ -1,7 +0,0 @@
-print("welcome to dead walk!")
-health=int(input("how much hp do you have"))
-age=input("how old are they")
-name=input("what is their name?")
-
-print(f'your character is {name} and is {age} years old with {health} hp!')
-print(f"nvm youre at{health*0.00000000000067} ")
