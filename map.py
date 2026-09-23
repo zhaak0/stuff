12c96a8986dce9ce923ad0979a8e84d3e7d5126d
zhaak0 <zakriya.khan@bcestudent.com>
2026-09-23 12:15:02 +0100

    Enhance map.py with moss and lab features
    Added random moss eating mechanic and lab interaction.

--- map.py
+++ map.py
@@ -1,5 +1,6 @@
-y=0
-x=0
+import random
+y=8
+x=5
 map = [["w","e","e","e","e","e","e","e","w"],
        ["w","f","f","f","f","f","f","f","w"],
        ["w","f","f","f","f","f","f","f","w"],
@@ -37,21 +38,37 @@ print("d - east")
 print("s - south")
 print("a - west")
 print("0 - stop moving")
-print("# means input movement")
+print("-> means input movement")
 
 while play==True:
-    dest = input("# ")
+    dest = input("-> ")
     current_tile = map[y][x]
     tilename = biom[current_tile]["t"]
-    print(current_tile)
-    print(x,y)
+
     if current_tile == "sh":
         print("there is a shop here.")
     elif current_tile == "w":
         print("there is a wall here.")
-    if dest == "0":
-        break
-    elif dest == "w":
+        rollmoss=random.randint(1,10)
+        if rollmoss==1:
+            mosstime=True
+            while mosstime==True:
+                eatmoss=input("there is moss here. eat it? Y/N\n")
+                if eatmoss.lower()=="y":
+                    print("you ate the mosss.\n you feel great!")
+                    mosstime=False
+                elif eatmoss.lower() == "n":
+                    print("loser.")
+                    mosstime=False
+
+    elif current_tile == "l":
+        labcount=0
+        labcount+=1
+        print("there is a lab here.")
+        if labcount==3:
+            print("okay try pressing w bro, get AWAY from the lab, please.")
+
+    if dest == "w":
         if y > 0:
             y-= 1
     elif dest == "d":
